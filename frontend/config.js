32087afdcad93ed4dc3a4b343297c9a8b44c195c
mk9487371@gmail.com <mk9487371@gmail.com>
2025-06-21 17:07:15 +0530

    errors solved on build

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,9 @@
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
+export const API = process.env.NODE_ENV === 'production'
+  ? process.env.NEXT_PUBLIC_API
+  : process.env.NEXT_PUBLIC_API_DEVELOPMENT;
 
-export const API = publicRuntimeConfig.PRODUCTION
-  ? publicRuntimeConfig.NEXT_PUBLIC_API
-  : publicRuntimeConfig.API_DEVELOPMENT;
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
 
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
-
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
-  ? publicRuntimeConfig.DOMAIN_PRODUCTION
-  : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+export const DOMAIN = process.env.NODE_ENV === 'production'
+  ? process.env.NEXT_PUBLIC_DOMAIN_PRODUCTION
+  : process.env.NEXT_PUBLIC_DOMAIN_DEVELOPMENT;
