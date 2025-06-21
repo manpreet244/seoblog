66b4e1850c2de8643387c864172b42b867ec63bd
mk9487371@gmail.com <mk9487371@gmail.com>
2025-06-21 16:32:31 +0530

    build error fixed

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? publicRuntimeConfig.API_PRODUCTION
+  ? publicRuntimeConfig.NEXT_PUBLIC_API
   : publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
