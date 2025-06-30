7bfc32e393483b92ec3fed384df26040164368a5
mk9487371@gmail.com <mk9487371@gmail.com>
2025-06-30 18:17:15 +0530

    config updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,5 @@
-import { publicRuntimeConfig } from "./next.config";
 
 export const API = process.env.NEXT_PUBLIC_API;
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
-export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME
+export const DISQUS_SHORTNAME = process.env.DISQUS_SHORTNAME
